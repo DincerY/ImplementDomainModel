d546df5431d42f07e94755b539212b4c0f598a44
Dinçer Yiğit <dinceryigit60@gmail.com>
2024-03-13 15:22:03 +0300

    Add project files.

---
+++ AnemicDomainModel/Program.cs
@@ -0,0 +1,84 @@
+﻿var productService = new ProductService();
+
+var laptop = productService.AddProduct("Laptop", 2000, 10);
+
+productService.UpdateStockQuantity(laptop, 5);
+
+productService.UpdatePrice(laptop, 2200);
+
+
+
+
+Console.ReadLine();
+
+
+
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+}
+
+public class ProductService
+{
+    public Product AddProduct(string name, decimal price, int stockQuantity)
+    {
+        ValidateInputs(name, price, stockQuantity);
+
+        var newProduct = new Product
+        {
+            ProductId = GenerateUniqueId(),
+            Name = name,
+            Price = price,
+            StockQuantity = stockQuantity
+        };
+
+        return newProduct;
+    }
+
+    public void UpdateStockQuantity(Product product, int newQuantity)
+    {
+        ValidateStockUpdate(newQuantity);
+
+        product.StockQuantity = newQuantity;
+    }
+
+    public void UpdatePrice(Product product, decimal newPrice)
+    {
+        ValidatePriceUpdate(newPrice);
+
+        product.Price = newPrice;
+    }
+
+    private void ValidateInputs(string name, decimal price, int stockQuantity)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Ürün adı geçerli değil.");
+
+        if (price <= 0)
+            throw new ArgumentException("Ürün fiyatı geçerli değil.");
+
+        if (stockQuantity < 0)
+            throw new ArgumentException("Ürün stok miktarı geçerli değil.");
+    }
+
+    private void ValidateStockUpdate(int newQuantity)
+    {
+        if (newQuantity < 0)
+            throw new InvalidOperationException("Geçersiz stok güncellemesi.");
+    }
+
+    private void ValidatePriceUpdate(decimal newPrice)
+    {
+        if (newPrice <= 0)
+            throw new InvalidOperationException("Geçersiz fiyat güncellemesi.");
+    }
+
+    private int GenerateUniqueId()
+    {
+        return new Random().Next();
+    }
+}
---
+++ RichDomainModel/Program.cs
@@ -0,0 +1,70 @@
+﻿
+var laptop = new Product("Laptop", 2200, 10);
+
+laptop.UpdateStockQuantity(5);
+
+laptop.UpdatePrice(2000);
+
+Console.ReadLine();
+
+
+public class Product
+{
+    public int ProductId { get; private set; }
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public int StockQuantity { get; private set; }
+
+    public Product(string name, decimal price, int stockQuantity)
+    {
+        ValidateInputs(name, price, stockQuantity);
+
+        ProductId = CreateIndetity();
+        Name = name;
+        Price = price;
+        StockQuantity = stockQuantity;
+    }
+
+    public void UpdateStockQuantity(int newQuantity)
+    {
+        ValidateStockUpdate(newQuantity);
+
+        StockQuantity = newQuantity;
+    }
+
+    public void UpdatePrice(decimal newPrice)
+    {
+        ValidatePriceUpdate(newPrice);
+
+        Price = newPrice;
+    }
+
+    private void ValidateInputs(string name, decimal price, int stockQuantity)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Ürün adı geçerli değil.");
+
+        if (price <= 0)
+            throw new ArgumentException("Ürün fiyatı geçerli değil.");
+
+        if (stockQuantity < 0)
+            throw new ArgumentException("Ürün stok miktarı geçerli değil.");
+    }
+
+    private void ValidateStockUpdate(int newQuantity)
+    {
+        if (newQuantity < 0)
+            throw new InvalidOperationException("Geçersiz stok güncellemesi.");
+    }
+
+    private void ValidatePriceUpdate(decimal newPrice)
+    {
+        if (newPrice <= 0)
+            throw new InvalidOperationException("Geçersiz fiyat güncellemesi.");
+    }
+
+    private int CreateIndetity()
+    {
+        return Random.Shared.Next();
+    }
+}
